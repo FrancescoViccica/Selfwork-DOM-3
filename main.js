b42d53b116a84041e5e673cb14b6883f2cc99268
Francesco Viccica <viccicafrancesco@gmail.com>
2025-06-23 16:31:09 +0200

    fase html e css conclusa

---
+++ main.js
@@ -0,0 +1,5 @@
+let input = document.querySelector('input');
+let playBtn = document.querySelector('#playBtn');
+let pauseBtn = document.querySelector('#pauseBtn');
+let stopBtn = document.querySelector('#stopBtn');
+
