01fcc87e51ce3d665d0332edae1b4fc5271f0355
Francesco Viccica <viccicafrancesco@gmail.com>
2025-06-24 10:01:13 +0200

    Esercizio svolto

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
-let input = document.querySelector('input');
+let inputCountdown = document.querySelector('#inputCountdown');
 let playBtn = document.querySelector('#playBtn');
 let pauseBtn = document.querySelector('#pauseBtn');
 let stopBtn = document.querySelector('#stopBtn');
+let remainingTime = document.querySelector('#remainingTime');
+
+
+let interval;
+let counter;
+let remainingSecond = 0;
+
+playBtn.addEventListener('click', ()=>{
+        
+        clearInterval(interval);
+        counter = inputCountdown.value;
+       
+
+        if (remainingSecond != 0) {
+            counter = remainingSecond
+        }
+    
+        interval = setInterval(() => {
+            if(counter < 0){
+                clearInterval(interval);
+                remainingTime.innerHTML = '--';
+    
+            }else{
+                
+                remainingTime.innerHTML = counter;
+                counter--
+            }
+            
+        }, 1000);
+    
+});
+
+pauseBtn.addEventListener('click', ()=>{
+    clearInterval(interval);
+    remainingSecond = counter;
+})
+
+stopBtn.addEventListener('click', ()=>{
+    clearInterval(interval);
+    inputCountdown.value = '';
+    remainingTime.innerHTML = '--';
+})
+
+
+
 
